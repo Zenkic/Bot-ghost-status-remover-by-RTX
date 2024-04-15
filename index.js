d2bbad1aa35e505a659e59f72596ce0175588a7b
Zenkic <166927974+Zenkic@users.noreply.github.com>
2024-04-15 18:04:22 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["**Watching Valorant Lietuva**🔥"];
 
 
 let currentIndex = 0;
